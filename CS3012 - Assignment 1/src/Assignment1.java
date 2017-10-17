1b82f3d12cecab1b711e08eefdad2ad858335e81
Finnegr1 <finnegr1@tcd.ie>
2017-10-17 14:23:24 +0100

    Commit 1: test

--- CS3012 - Assignment 1/src/Assignment1.java
+++ CS3012 - Assignment 1/src/Assignment1.java
@@ -1,4 +1,22 @@
 
-public class Assignment1 {
 
+public class BST <Key extends Comparable<Key>>{
+	
+	//Private class representing Nodes in a BST
+	private class Node{
+		Node left, right;
+		Key key;
+		
+		public Node(Key key){
+			this.key = key;
+		}
+	}
+	
+	//Root node (node with no parent node)
+	private Node root;
+	
+	//
+	public BST(){
+		
+	}
 }
